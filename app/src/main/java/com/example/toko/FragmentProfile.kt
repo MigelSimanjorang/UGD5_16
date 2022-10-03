2742e69df34935d76e3a6e197234e58bb9345cd5
LintahLaut <fernanditoppo@gmail.com>
2022-10-04 00:04:04 +0700

    fragment profile jadi om

--- app/src/main/java/com/example/toko/FragmentProfile.kt
+++ app/src/main/java/com/example/toko/FragmentProfile.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.example.toko.databinding.FragmentProfileBinding
 import com.example.toko.room.SepatuDB
 
@@ -18,40 +19,41 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class FragmentProfile : Fragment() {
+class FragmentProfile: Fragment() {
+
     val db by lazy { activity?.let { SepatuDB(it) } }
+    private val myPreference = "login"
     private val id = "idKey"
-    private val myPreference = "myPref"
     var sharedPreferences: SharedPreferences? = null
-    private var _binding: FragmentProfileBinding? = null
-    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         sharedPreferences = activity?.getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+        val showUsername: TextView = view.findViewById(R.id.username)
+        val showEmail: TextView = view.findViewById(R.id.email)
+        val showTanggal: TextView = view.findViewById(R.id.tanggalLahir)
+        val showNomorHP: TextView = view.findViewById(R.id.noTelepon)
 
         CoroutineScope(Dispatchers.IO).launch {
             val user = db?.userDao()?.getUser(sharedPreferences!!.getString(id,"")!!.toInt())?.get(0)
-            binding.nama.setText(user?.username)
-            binding.email.setText(user?.email)
-            binding.noTelp.setText(user?.noTelepon)
-            binding.tglLahir.setText(user?.tanggalLahir)
-            binding.btnEditData.setOnClickListener {
-                val moveEdit = Intent(activity, EditSepatuActivity::class.java)
-                startActivity(moveEdit)
-                activity?.finish()
-            }
+            showUsername.setText(user?.username)
+            showEmail.setText(user?.email)
+            showTanggal.setText(user?.tanggalLahir)
+            showNomorHP.setText(user?.noTelepon)
+        }
+
+        btnEditData.setOnClickListener() {
+            val intent = Intent(context, EditSepatuActivity::class.java)
+            startActivity(intent)
         }
     }
-
-
 }
