e958d5108b1394c69024ed66fc8aa459a69202d8
MigelSimanjorang <112043487+MigelSimanjorang@users.noreply.github.com>
2022-10-03 23:05:17 +0700

    update om

--- app/src/main/java/com/example/toko/FragmentProfile.kt
+++ app/src/main/java/com/example/toko/FragmentProfile.kt
@@ -1,59 +1,58 @@
 package com.example.toko
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.toko.databinding.FragmentProfileBinding
+import com.example.toko.room.UserDB
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentProfile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentProfile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val db by lazy { activity?.let { UserDB(it) } }
+    private val id = "idKey"
+    private val myPreference = "myPref"
+    var sharedPreferences: SharedPreferences? = null
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentProfile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentProfile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnEditData : Button = view.findViewById(R.id.btnEditData)
+        sharedPreferences = activity?.getSharedPreferences(myPreference, Context.MODE_PRIVATE)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val family = db?.userDao()?.(sharedPreferences!!.getString(id,"")!!.toInt())?.get(0)
+            binding.nama.setText(family?.nama)
+            binding.email.setText(family?.emaill)
+            binding.noTelp.setText(family?.noTelepon)
+            binding.tglLahir.setText(family?.TanggalLahir)
+
+            binding.btnEditData.setOnClickListener {
+                val moveEdit = Intent(this@FragmentProfile, FragmentEditProfile::class.java)
+                startActivity(moveEdit)
+                activity?.finish()
             }
+        }
     }
+
+
 }
